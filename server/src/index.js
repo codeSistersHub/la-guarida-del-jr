55312009a8cc1ab30771cd2e8933b0251bc5c4ad
Lourdesjupo <lourdes.jupo@gmail.com>
2024-06-18 17:53:41 +0200

    Add server and connection with postgresql

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,52 @@
-//Imports
+// const express = require('express');
+// const cors = require('cors');
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors'
+import express from 'express'
+import postgres from 'postgres';
 
-import express, { json } from 'express';
-import cors from 'cors';
+
 //Arranque servidor
 
-const server = express();
+const server = express()
 
 //configuración de servidor
 
-server.use(cors());
-server.use(json({ limit: '10mb' }));
+server.use(cors())
+server.use(express.json({limit:"10mb"}))
+//listening
 
-const port = 3000
-server.listen(port, () => {
-  console.log(`servidor arrancado: http://localhost:${port}`);
-});
+const port = 4500;
+server.listen(port, ()=>{ console.log (`servidor arrancado: http://localhost:${port}`)})
+
+//conexión a BBDD
+const connectionString = process.env.DATABASE_URL;
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+if (!connectionString) {
+  console.error('DATABASE_URL no está definida');
+	process.exit(1);
+}
+
+const sql = postgres(connectionString,
+  {host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE}
+);
 
 
 
+async function getUsersOver() {
+  try{
+	const users = await sql`
+    select user_name, email_name
+    from users
+  `;
+  console.log(users);
+  } catch(err){
+    console.log(err)
+  }
+}
+
+getUsersOver()
