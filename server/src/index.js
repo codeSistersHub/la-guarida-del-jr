72a4ad69f5df256da2a5e0a2c9d6608bc4b5e98d
Aidablaya <you@example.com>
2024-07-08 09:43:49 +0200

    add request get

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
 import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors'
@@ -50,3 +48,57 @@ async function getUsersOver() {
 }
 
 getUsersOver()
+//GET
+
+server.get('/api/users', async (req, res) => {
+  try {
+    const users = await sql`
+      SELECT user_name, email_name
+      FROM users
+    `;
+    res.json(users);
+  } catch (err) {
+    console.error('Error al obtener usuarios:', err);
+    res.status(500).json({ error: 'Error al obtener usuarios' });
+  }
+})
+
+server.get('/api/threads', async (req, res) => {
+  console.log('Solicitud recibida en /api/threads'); // Log inicial
+
+  const { id_section } = req.query;
+  console.log('Parámetro id_section:', id_section); // Log para verificar el id_section
+
+  // Verificar si se ha proporcionado el parámetro id_section
+  if (!id_section) {
+    console.log('Falta el parámetro id_section'); // Log para verificar falta de parámetro
+    return res.status(400).json({ error: 'Se requiere el parámetro id_section' });
+  }
+ 
+  try {
+    const publish = await sql`
+      SELECT 
+        id_publish, 
+        name_user, 
+        fkid_section, 
+        title_publish, 
+        url_job, 
+        url_linkedIn, 
+        fk_id_tag, 
+        votesSmile, 
+        votesSad, 
+        votesHeart, 
+        fk_id_user, 
+        num_responses, 
+        isfav 
+      FROM publish 
+      WHERE fkid_section = ${id_section}
+    `;
+    console.log('Resultados de la consulta:', publish); // Log para verificar los resultados de la consulta
+    res.json(publish);
+  } catch (err) {
+    console.error('Error al obtener publicaciones:', err);
+    res.status(500).json({ error: 'Error al obtener publicaciones' });
+  }
+});
+
