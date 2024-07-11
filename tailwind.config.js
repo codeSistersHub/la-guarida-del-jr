4e48e65ce5a60cc10c55a0f16a437a054b2fba8d
MariaPeCa <mpenacantero@gmail.com>
2024-07-11 12:21:36 +0200

    add colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				siswhite: '#e3e3e3',
+				sisblack: '#121212',
+				accentblue: '#406cd6',
+				light_msgbg: '#d9e2f7', 
+				light_bar: '#f6f6f6',
+				light_addbtn: '#1d3161',
+				light_login:'#8eaae9', 
+				light_tags: '#8eaae9',
+				light_footer: '#d9e2f7',
+				dark_bg: '#1d3161',
+				dark_msgbg:'#406cd6',
+				dark_addbtn: '#8eaae9',
+				dark_bar: '#48587c',
+				dark_footer:'#243c78',
+				dark_userpass: '#243c78',
+				taggreen:'#d5eee8',
+				tagyellow:'#ebefd8',
+				taglilac: '#d7def0'
+			}
+		},
 	},
 	plugins: [],
 };
