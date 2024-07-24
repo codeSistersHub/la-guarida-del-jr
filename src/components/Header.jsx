a8b297e8ef97ee0a95d20e02d5397dfd348795fa
AstracodeLAB <astracodelab@gmail.com>
2024-07-24 16:24:16 +0200

    adsa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
 import Menu from "./Menu"
-=======
-import React from 'react'
-import Menu from './Menu'
-/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@awesome.me/kit-KIT_CODE/icons/classic/solid'*/
->>>>>>> 7d3c69758f654be1423c331c3f6c638b7c0238d7
 
 function Header() {
   return (
     <>
       <h1>header</h1>
-<<<<<<< HEAD
       <Menu />
-=======
-    <Menu />
-    ¨
-    {/* <FontAwesomeIcon icon={fa-user} /> */}
->>>>>>> 7d3c69758f654be1423c331c3f6c638b7c0238d7
     </>
   )
 }
