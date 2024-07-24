a644417e54fe6b285301ee1834951e9c86ef7321
AstracodeLAB <astracodelab@gmail.com>
2024-07-24 16:25:21 +0200

    asdad

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,16 +6,4 @@ const Menu = ({ isOpen, toggleMenu }) => {
 	);
 };
 
-<<<<<<< HEAD
 export default Menu;
-=======
-function Menu() {
-  return (
-    <div>
-      <h1>Menu</h1>
-    </div>
-  )
-}
-
-export default Menu
->>>>>>> 7d3c69758f654be1423c331c3f6c638b7c0238d7
